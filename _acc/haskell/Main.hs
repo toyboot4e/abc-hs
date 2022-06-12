cc326e2c9b758e1074938029615ebbb9dabce97d
toyboot4e <toyboot4e@gmail.com>
2022-06-12 12:56:46 +0900

    use `atcoder-cli`

--- _acc/haskell/Main.hs
+++ _acc/haskell/Main.hs
@@ -1,9 +1,12 @@
-module Main (main) where
+#!/usr/bin/env runghc
 
-solve :: [Int] -> Int
-solve xs = undefined
+module Main (main) where
 
 main :: IO ()
 main = do
   xs <- map read . words <$> getLine :: IO [Int]
   print $ solve xs
+
+solve :: [Int] -> Int
+solve xs = undefined
+
