81d88a12204ecee341a7debb49d9b3c138c9826f
toyboot4e <toyboot4e@gmail.com>
2022-06-21 21:54:08 +0900

    abc256 d: steal answer and win

--- abc256/d/Main.hs
+++ abc256/d/Main.hs
@@ -58,26 +58,23 @@ main :: IO ()
 main = do
   n <- getLineInt
   rs <- replicateM n getLineInts2
-  let res = solve rs
-      bs = map (intTupleToBsb $ BSB.char7 ' ') res
-   in BSB.hPutBuilder stdout $
-        (mconcat $ intersperse (BSB.char7 '\n') bs) <> (BSB.char7 '\n')
+
+  let rs' = sortBy (\(!l, !_) (!l', !_) -> compare l l') rs
+  let res = solve2 rs'
+
+  let bs = map (intTupleToBsb $ BSB.char7 ' ') res
+  BSB.hPutBuilder stdout $
+    (mconcat $ intersperse (BSB.char7 '\n') bs) <> (BSB.char7 '\n')
 
 intTupleToBsb :: BSB.Builder -> (Int, Int) -> BSB.Builder
 -- TODO: use tuple
 intTupleToBsb c xs = mconcat $ intersperse c $ [BSB.intDec $ fst xs, BSB.intDec $ snd xs]
 
-solve :: [(Int, Int)] -> [(Int, Int)]
-solve rs =
-  let rs' = sortBy (\(!l, !_) (!l', !_) -> compare l l') rs
-   in foldl mergeMany [head rs'] (tail rs')
-
-mergeMany :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
-mergeMany rs r1@(!_, !_) = concat $ map (\ !r0 -> merge r0 r1) rs
-
-merge :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
-merge r0@(!l, !r) r1@(!l', !r') =
-  if
-      | l <= l' && l' <= r -> [(l, r `max` r')]
-      | l' <= l && l <= r' -> [(l', r `max` r')]
-      | otherwise -> [r0, r1]
+solve2 :: [(Int, Int)] -> [(Int, Int)]
+solve2 [] = []
+solve2 ((!x, !y) : rs) = loop x y rs
+  where
+    loop !x !y [] = [(x, y)]
+    loop !x !y ((u, v) : rs)
+      | u <= y = loop x (max y v) rs
+      | otherwise = (x, y) : loop u v rs
