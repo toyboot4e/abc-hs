0640cec0f111b08a4ddc7095b69288159577cb1f
toyboot4e <toyboot4e@gmail.com>
2022-07-23 23:37:43 +0900

    update template

--- _acc/haskell/Main.hs
+++ _acc/haskell/Main.hs
@@ -1,30 +1,58 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-16.11 --package bytestring --package vector
+-- stack script --resolver lts-16.11 --package bytestring --package vector --package containers
+
+-- {{{ Imports
 
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MultiWayIf #-}
 
 module Main (main) where
 
+import Control.Applicative
 import Control.Monad
-import qualified Data.ByteString.Builder as BSB
-import qualified Data.ByteString.Char8 as BS
+import Control.Monad.Fix
 import Data.Char
 import Data.List
 import Data.Maybe
 import Data.Ord
-import qualified Data.Vector.Fusion.Bundle as VFB
-import qualified Data.Vector.Generic as VG
-import qualified Data.Vector.Unboxed as VU
 import GHC.Event (IOCallback)
 import GHC.Float (int2Float)
 import System.IO
 import Text.Printf
 
-unreachable = error "unreachable"
+{- ORMOLU_DISABLE -}
 
-sortWith :: Ord o => (a -> o) -> [a] -> [a]
-sortWith = sortBy . comparing
+-- bytestring
+import qualified Data.ByteString.Builder as BSB
+import qualified Data.ByteString.Char8 as BS
+
+-- vector
+import qualified Data.Vector.Fusion.Bundle as VFB
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Unboxed as VU
+
+-- containers
+import qualified Data.IntMap.Strict as IM
+import qualified Data.Map.Strict as M
+
+{- ORMOLU_ENABLE -}
+
+-- Option - Maybe cheatsheet
+-- https://notes.iveselov.info/programming/cheatsheet-rust-option-vs-haskell-maybe
+
+-- - filter_map = mapMaybe
+
+-- }}}
+
+-- {{{ Template
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (a : as) = Just a
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (a : as) = Just as
 
 sortWithDesc :: Ord o => (a -> o) -> [a] -> [a]
 sortWithDesc = sortBy . flip . comparing
@@ -42,10 +70,13 @@ getLineInts = bsToInts <$> BS.getLine
 vLength :: (VG.Vector v e) => v e -> Int
 vLength = VFB.length . VG.stream
 
+-- }}}
+
 main :: IO ()
 main = do
   xs <- getLineInts
   print $ solve xs
 
 solve :: [Int] -> Int
-solve xs = undefined
+solve xs =
+  undefined
