6e1a9fb01cef0ff1dd175d210fe7d5d6acb7f164
toyboot4e <toyboot4e@gmail.com>
2022-11-05 11:08:55 +0900

    update template

--- _acc/haskell/Main.hs
+++ _acc/haskell/Main.hs
@@ -1,73 +1,104 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-16.11 --package bytestring --package vector --package containers
+-- stack script --resolver lts-16.11 --package bytestring --package vector --package vector-algorithms --package containers --package array
 
 -- {{{ Imports
-
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Fix
+import Control.Monad.ST
+import Data.Array
+import Data.Bifunctor
+import Data.Bits
 import Data.Char
+import Data.IORef
 import Data.List
 import Data.Maybe
 import Data.Ord
 import GHC.Event (IOCallback)
+import GHC.Exts
 import GHC.Float (int2Float)
 import System.IO
 import Text.Printf
 
 {- ORMOLU_DISABLE -}
 
--- bytestring
+-- bytestring: https://www.stackage.org/lts-16.11/package/bytestring-0.10.10.0
 import qualified Data.ByteString.Builder as BSB
 import qualified Data.ByteString.Char8 as BS
 
--- vector
+-- vector: https://www.stackage.org/lts-16.11/package/vector-0.12.1.2
 import qualified Data.Vector.Fusion.Bundle as VFB
 import qualified Data.Vector.Generic as VG
 import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
 
--- containers
+-- vector-algorithms: https://www.stackage.org/haddock/lts-16.11/vector-algorithms-0.8.0.3/Data-Vector-Algorithms-Intro.html
+import qualified Data.Vector.Algorithms.Intro as VAI
+
+-- containers: https://www.stackage.org/lts-16.11/package/containers-0.6.2.1
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
 
 {- ORMOLU_ENABLE -}
 
+-- }}}
+
+-- {{{ cheatsheet
+
 -- Option - Maybe cheatsheet
 -- https://notes.iveselov.info/programming/cheatsheet-rust-option-vs-haskell-maybe
 
--- - filter_map = mapMaybe
+-- safe list access
+-- safeHead :: [a] -> Maybe a
+-- safeHead [] = Nothing
+-- safeHead (a : as) = Just a
+--
+-- safeTail :: [a] -> Maybe [a]
+-- safeTail [] = Nothing
+-- safeTail (a : as) = Just as
+
+-- sortWith
+--
+-- sortWithDesc :: Ord o => (a -> o) -> [a] -> [a]
+-- sortWithDesc = sortBy . flip . comparing
+--
+-- maximumWith :: Foldable t => Ord o => (a -> o) -> t a -> a
+-- maximumWith = maximumBy . comparing
+--
+-- minimumWith :: Foldable t => Ord o => (a -> o) -> t a -> a
+-- minimumWith = minimumBy . comparing
 
 -- }}}
 
 -- {{{ Template
 
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing
-safeHead (a : as) = Just a
+-- compress duduplicates sorted list, nub deduplicates non-sorted list
+-- TODO: std?
+compress [] = []
+compress (x : xs) = x : (compress $ dropWhile (== x) xs)
 
-safeTail :: [a] -> Maybe [a]
-safeTail [] = Nothing
-safeTail (a : as) = Just as
-
-sortWithDesc :: Ord o => (a -> o) -> [a] -> [a]
-sortWithDesc = sortBy . flip . comparing
-
-getLineInt :: IO Int
-getLineInt = fst . fromJust . BS.readInt <$> BS.getLine
-
-bsToIntList :: BS.ByteString -> [Int]
-bsToIntList = unfoldr (BS.readInt . BS.dropWhile isSpace)
-
-bsToIntVec :: BS.ByteString -> VU.Vector Int
-bsToIntVec = VU.unfoldr (BS.readInt . BS.dropWhile isSpace)
+-- e.g. binary ocombinations:
+-- combination 2 [0..8]
+combinations :: Int -> [a] -> [[a]]
+combinations n xs = comb n (length xs) xs
+  where
+    comb 0 _ _ = [[]]
+    comb r n a@(x : xs)
+      | n == r = [a]
+      | otherwise = map (x :) (comb (r - 1) (n - 1) xs) ++ comb r (n - 1) xs
 
 getLineIntList :: IO [Int]
-getLineIntList = bsToIntList <$> BS.getLine
+getLineIntList = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 
 getLineIntVec :: IO (VU.Vector Int)
 getLineIntVec = VU.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
@@ -80,12 +111,62 @@ vLength = VFB.length . VG.stream
 vRange :: Int -> Int -> VU.Vector Int
 vRange i j = VU.enumFromN i (j + 1 - i)
 
-combinations :: Int -> [a] -> [[a]]
-combinations n xs = comb n (length xs) xs where
-  comb 0 _ _ = [[]]
-  comb r n a@(x:xs)
-    | n == r    = [a]
-    | otherwise = map (x:) (comb (r - 1) (n - 1) xs) ++ comb r (n - 1) xs
+-- | Binary search for a sorted items which returns the `(ok, ng)` index pair at the boundary.
+-- |
+-- | Example
+-- | -------
+-- |
+-- | With an OK predicate `(<= 5)`, list `[0..9]` can be seen as:
+-- |
+-- | > [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+-- | >  <-------------->  <-------->
+-- | >         ok             ng
+-- |
+-- | In this case `bsearchHelper` returns the `(ok, ng)` = `(5, 6)` pair:
+-- |
+-- | > > let xs = [0..9] in do
+-- | > >   print $ bsearchHelper (0, 9) (\i -> xs !! i <= 5)
+-- | > (5, 6)
+-- |
+-- | Note that the closure CANNOT BE an exact match. Use a range-based predicate instead.
+-- |
+-- | Errors in edge cases
+-- | --------------------
+-- |
+-- | While this function works for the above example, be warned that it's incomplete.
+-- | It makes errors in certain edge cases:
+-- |
+-- | 1. The only `ok` item is at the end of the list.
+-- | 2. The only `ng` item is at the beginning of the list.
+-- | 3. There's no `ok` item or there's no `ng` item.
+-- |
+-- | So this function is wrapped by `bsearch`.
+bsearchHelper :: (Int, Int) -> (Int -> Bool) -> (Int, Int)
+bsearchHelper (ok, ng) isOk
+  | abs (ok - ng) == 1 = (ok, ng)
+  | isOk m = bsearchHelper (m, ng) isOk
+  | otherwise = bsearchHelper (ok, m) isOk
+  where
+    m = (ok + ng) `div` 2
+
+-- | Binary search for inclusive range (left to right only)
+bsearch :: (Int, Int) -> (Int -> Bool) -> (Maybe Int, Maybe Int)
+bsearch (low, top) isOk = bimap wrap wrap result
+  where
+    ok = low - 1
+    ng = top + 1
+    result = bsearchHelper (ok, ng) isOk'
+
+    isOk' :: Int -> Bool
+    isOk' x
+      | x == low = True
+      | x == top = False
+      | otherwise = isOk x
+
+    wrap :: Int -> Maybe Int
+    wrap x
+      | x == low || x == top = Nothing
+      | otherwise = Just x
 
 -- }}}
 
