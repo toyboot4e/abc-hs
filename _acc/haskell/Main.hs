e9e37923ade739e5e5e7f071e21cfa846833053c
toyboot4e <toyboot4e@gmail.com>
2022-11-06 21:41:40 +0900

    update Union-Find template

--- _acc/haskell/Main.hs
+++ _acc/haskell/Main.hs
@@ -1,18 +1,16 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-16.11 --package bytestring --package vector --package vector-algorithms --package containers --package array
+-- stack script --resolver lts-16.11 --package bytestring --package vector --package vector-algorithms --package containers --package array --package primitive
+
+{-# LANGUAGE BangPatterns, MultiWayIf, NumericUnderscores, ScopedTypeVariables, TypeApplications #-}
 
 -- {{{ Imports
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Fix
+import Control.Monad.Primitive
 import Control.Monad.ST
 import Data.Array
 import Data.Bifunctor
@@ -142,13 +140,21 @@ bsearch (low, top) isOk = bimap wrap wrap result
 -- {{{ Union-Find
 
 -- | Union-find implementation (originally by `@pel`)
-newtype UnionFind m = UnionFind (VM.MVector (PrimState m) UfNode)
+newtype UnionFind s = UnionFind (VM.MVector s UfNode)
 
--- | `Child parent | Root size`
--- | I failed to implement `Unbox` for this algebric data type..
+type IOUnionFind = UnionFind RealWorld
+type STUnionFind s = UnionFind s
+
+-- | `Child parent | Root size`. Not `Unbox` :(
 data UfNode = Child {-# UNPACK #-} !Int | Root {-# UNPACK #-} !Int
 
-root :: (PrimMonad m) => UnionFind m -> Int -> m Int
+-- | Creates a new Union-Find tree of the given size.
+newUF :: (PrimMonad m) => Int -> m (UnionFind (PrimState m))
+newUF n = UnionFind <$> VM.replicate n (Root 1)
+
+-- | Returns the root node index.
+{-# INLINE root #-}
+root :: (PrimMonad m) => UnionFind (PrimState m) -> Int -> m Int
 root uf@(UnionFind vec) i = do
   node <- VM.read vec i
   case node of
@@ -159,26 +165,33 @@ root uf@(UnionFind vec) i = do
       VM.write vec i (Child r)
       return r
 
-same :: (PrimMonad m) => UnionFind m -> Int -> Int -> m Bool
+-- | Checks if the two nodes are under the same root.
+{-# INLINE same #-}
+same :: (PrimMonad m) => UnionFind (PrimState m) -> Int -> Int -> m Bool
 same uf x y = liftM2 (==) (root uf x) (root uf y)
 
+-- | Just an internal helper.
 unwrapRoot :: UfNode -> Int
 unwrapRoot (Root s) = s
 unwrapRoot (Child _) = undefined
 
-unite :: (PrimMonad m) => UnionFind m -> Int -> Int -> m ()
+-- | Unites two nodes.
+{-# INLINE unite #-}
+unite :: (PrimMonad m) => UnionFind (PrimState m) -> Int -> Int -> m ()
 unite uf@(UnionFind vec) x y = do
   px <- root uf x
   py <- root uf y
   when (px /= py) $ do
     sx <- unwrapRoot <$> VM.read vec px
     sy <- unwrapRoot <$> VM.read vec py
-    -- NOTE(perf): union by rank (choose smaller one for root on)
+    -- NOTE(perf): union by rank (choose smaller one for root)
     let (par, chld) = if sx < sy then (px, py) else (py, px)
     VM.write vec chld (Child par)
     VM.write vec par (Root (sx + sy))
 
-size :: (PrimMonad m) => UnionFind m -> Int -> m Int
+-- | Returns the size of the root node, starting with `1`.
+{-# INLINE size #-}
+size :: (PrimMonad m) => UnionFind (PrimState m) -> Int -> m Int
 size uf@(UnionFind vec) x = do
   px <- root uf x
   s <- unwrapRoot <$> VM.read vec px
