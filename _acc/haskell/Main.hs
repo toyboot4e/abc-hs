9ad1bfec665be98bc2ec4e5efcdf2e15b6b5e02a
toyboot4e <toyboot4e@gmail.com>
2022-11-06 19:54:00 +0900

    wip Union-Find template

--- _acc/haskell/Main.hs
+++ _acc/haskell/Main.hs
@@ -80,7 +80,113 @@ import qualified Data.Map.Strict as M
 
 -- }}}
 
--- {{{ Template
+-- {{{ Binary search
+
+-- | Binary search for a sorted items which returns the `(ok, ng)` index pair at the boundary.
+-- |
+-- | Example
+-- | -------
+-- |
+-- | With an OK predicate `(<= 5)`, list `[0..9]` can be seen as:
+-- |
+-- | > [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+-- | >  <-------------->  <-------->
+-- | >         ok             ng
+-- |
+-- | In this case `bsearchMain` returns the `(ok, ng)` = `(5, 6)` pair:
+-- |
+-- | > > let xs = [0..9] in do
+-- | > >   print $ bsearchMain (0, 9) (\i -> xs !! i <= 5)
+-- | > (5, 6)
+-- |
+-- | Note that the closure CANNOT BE an exact match. Use a range-based predicate instead.
+-- |
+-- | Errors in edge cases
+-- | --------------------
+-- |
+-- | While this function works for the above example, be warned that it's incomplete.
+-- | It makes errors in certain edge cases:
+-- |
+-- | 1. The only `ok` item is at the end of the list.
+-- | 2. The only `ng` item is at the beginning of the list.
+-- | 3. There's no `ok` item or there's no `ng` item.
+-- |
+-- | So this function is wrapped by `bsearch`.
+bsearchMain :: (Int, Int) -> (Int -> Bool) -> (Int, Int)
+bsearchMain (ok, ng) isOk
+  | abs (ok - ng) == 1 = (ok, ng)
+  | isOk m = bsearchMain (m, ng) isOk
+  | otherwise = bsearchMain (ok, m) isOk
+  where
+    m = (ok + ng) `div` 2
+
+-- | Binary search for inclusive range (from left to right only)
+bsearch :: (Int, Int) -> (Int -> Bool) -> (Maybe Int, Maybe Int)
+bsearch (low, top) isOk = bimap wrap wrap result
+  where
+    result = bsearchMain (low - 1, top + 1) isOk'
+
+    isOk' :: Int -> Bool
+    isOk' x
+      | x == low - 1 = True
+      | x == top + 1 = False
+      | otherwise = isOk x
+
+    wrap :: Int -> Maybe Int
+    wrap x
+      | x == low - 1 || x == top + 1 = Nothing
+      | otherwise = Just x
+
+-- }}}
+
+-- {{{ Union-Find
+
+-- | Union-find implementation (originally by `@pel`)
+newtype UnionFind m = UnionFind (VM.MVector (PrimState m) UfNode)
+
+-- | `Child parent | Root size`
+-- | I failed to implement `Unbox` for this algebric data type..
+data UfNode = Child {-# UNPACK #-} !Int | Root {-# UNPACK #-} !Int
+
+root :: (PrimMonad m) => UnionFind m -> Int -> m Int
+root uf@(UnionFind vec) i = do
+  node <- VM.read vec i
+  case node of
+    Root _ -> return i
+    Child p -> do
+      r <- root uf p
+      -- NOTE(perf): path compression (move the queried node to just under the root, recursivelly)
+      VM.write vec i (Child r)
+      return r
+
+same :: (PrimMonad m) => UnionFind m -> Int -> Int -> m Bool
+same uf x y = liftM2 (==) (root uf x) (root uf y)
+
+unwrapRoot :: UfNode -> Int
+unwrapRoot (Root s) = s
+unwrapRoot (Child _) = undefined
+
+unite :: (PrimMonad m) => UnionFind m -> Int -> Int -> m ()
+unite uf@(UnionFind vec) x y = do
+  px <- root uf x
+  py <- root uf y
+  when (px /= py) $ do
+    sx <- unwrapRoot <$> VM.read vec px
+    sy <- unwrapRoot <$> VM.read vec py
+    -- NOTE(perf): union by rank (choose smaller one for root on)
+    let (par, chld) = if sx < sy then (px, py) else (py, px)
+    VM.write vec chld (Child par)
+    VM.write vec par (Root (sx + sy))
+
+size :: (PrimMonad m) => UnionFind m -> Int -> m Int
+size uf@(UnionFind vec) x = do
+  px <- root uf x
+  s <- unwrapRoot <$> VM.read vec px
+  return s
+
+-- }}}
+
+-- {{{ Misc
 
 -- compress duduplicates sorted list, nub deduplicates non-sorted list
 -- TODO: std?
@@ -110,63 +216,6 @@ vLength = VFB.length . VG.stream
 {-# INLINE vRange #-}
 vRange :: Int -> Int -> VU.Vector Int
 vRange i j = VU.enumFromN i (j + 1 - i)
-
--- | Binary search for a sorted items which returns the `(ok, ng)` index pair at the boundary.
--- |
--- | Example
--- | -------
--- |
--- | With an OK predicate `(<= 5)`, list `[0..9]` can be seen as:
--- |
--- | > [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
--- | >  <-------------->  <-------->
--- | >         ok             ng
--- |
--- | In this case `bsearchHelper` returns the `(ok, ng)` = `(5, 6)` pair:
--- |
--- | > > let xs = [0..9] in do
--- | > >   print $ bsearchHelper (0, 9) (\i -> xs !! i <= 5)
--- | > (5, 6)
--- |
--- | Note that the closure CANNOT BE an exact match. Use a range-based predicate instead.
--- |
--- | Errors in edge cases
--- | --------------------
--- |
--- | While this function works for the above example, be warned that it's incomplete.
--- | It makes errors in certain edge cases:
--- |
--- | 1. The only `ok` item is at the end of the list.
--- | 2. The only `ng` item is at the beginning of the list.
--- | 3. There's no `ok` item or there's no `ng` item.
--- |
--- | So this function is wrapped by `bsearch`.
-bsearchHelper :: (Int, Int) -> (Int -> Bool) -> (Int, Int)
-bsearchHelper (ok, ng) isOk
-  | abs (ok - ng) == 1 = (ok, ng)
-  | isOk m = bsearchHelper (m, ng) isOk
-  | otherwise = bsearchHelper (ok, m) isOk
-  where
-    m = (ok + ng) `div` 2
-
--- | Binary search for inclusive range (left to right only)
-bsearch :: (Int, Int) -> (Int -> Bool) -> (Maybe Int, Maybe Int)
-bsearch (low, top) isOk = bimap wrap wrap result
-  where
-    ok = low - 1
-    ng = top + 1
-    result = bsearchHelper (ok, ng) isOk'
-
-    isOk' :: Int -> Bool
-    isOk' x
-      | x == low = True
-      | x == top = False
-      | otherwise = isOk x
-
-    wrap :: Int -> Maybe Int
-    wrap x
-      | x == low || x == top = Nothing
-      | otherwise = Just x
 
 -- }}}
 
