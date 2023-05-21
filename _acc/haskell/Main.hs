f9c59affa39bb50f79a093fad9f007eb6a447c05
toyboot4e <toyboot4e@gmail.com>
2023-05-21 23:34:21 +0900

    add lazy segment tree

--- _acc/haskell/Main.hs
+++ _acc/haskell/Main.hs
@@ -2132,6 +2132,234 @@ addFlowRNEdge !rn !v1 !v2 !flow = do
 
 -- }}}
 
+-- {{{ Lazy segment tree
+
+class (Monoid op) => MonoidAction op a where
+  -- | Performs semigroup action over the target set `a`.
+  mact :: op -> a -> a
+
+-- TODO: Do we need to duplicate `SegmentTree` and `LazySegmentTree`?
+-- TODO: Use generic vector type
+-- TODO: We're assuming communicative operator monoid in LazySegmentTree, right?
+-- TODO: Vertex -> Node
+
+-- | Lazy segment tree.
+-- |
+-- | # Indices
+-- |
+-- | Use 1-based indices for super handy index hacks:
+-- |
+-- | ```txt
+-- |            1             |
+-- |      2           3       | height = 4
+-- |   4     5     6     7    |
+-- | 08 09 10 11 12 13 14 15  v
+-- | ^
+-- | +-- nVerts `div` 2
+-- |
+-- | 0  1  2  3  4  5  6  7   -- iLeaf is given by user and uses zero-based indices.
+-- |
+-- | - parentVertex = vertex `div` 2 = shiftR vertex 1
+-- | - leftChild = vertex * 2 = shiftR vertex 1
+-- | - rightChild = vertex * 2 + 1 = shiftR vertex 1 + 1 = (shiftR vertex 1) .|. 1
+-- |
+-- | ```
+data LazySegmentTree a op s = LazySegmentTree !(VUM.MVector s a) !(VUM.MVector s op) !Int
+
+newLazySTree ::
+  forall a op m.
+  (Monoid a, MonoidAction op a, VU.Unbox a, VU.Unbox op, PrimMonad m) =>
+  Int ->
+  m (LazySegmentTree a op (PrimState m))
+newLazySTree !n = do
+  !as <- VUM.replicate n2 mempty
+  !ops <- VUM.replicate n2 mempty
+  return $ LazySegmentTree as ops h
+  where
+    -- TODO: use bit operations
+    (!h, !n2) = until ((>= 2 * n) . snd) (bimap succ (* 2)) (0 :: Int, 1 :: Int)
+
+-- | Propagates the lazy operator monoids from top to bottom where the laef vertex is contained.
+-- |
+-- | - `iLeaf`: Given with zero-based index.
+_propOpMonoidsToLeaf ::
+  (Monoid a, MonoidAction op a, Eq op, VU.Unbox a, VU.Unbox op, PrimMonad m) =>
+  LazySegmentTree a op (PrimState m) ->
+  Int ->
+  m ()
+_propOpMonoidsToLeaf (LazySegmentTree !as !ops !height) !iLeaf = do
+  let !leafVertex = iLeaf + nVerts `div` 2
+
+  -- From parent vertex to the parent of the leaf vertex:
+  forMS_ (rangeMSR 1 (pred height)) $ \iParent -> do
+    let !vertex = nthParent leafVertex iParent
+
+    -- When there's some lazy evaluation value, propagate them to their children and evaluate the vertex:
+    !op <- VUM.read ops vertex
+    when (op /= mempty) $ do
+      -- Propagate the operator monoid to the children:
+      VUM.modify ops (<> op) $ childL vertex
+      VUM.modify ops (<> op) $ childR vertex
+
+      -- Evaluate the vertex and consume the operator monoid:
+      VUM.modify as (mact op) vertex
+      VUM.write ops vertex mempty
+
+  where
+    !nVerts = VUM.length as
+    nthParent !leafVertex !nth = shiftR leafVertex nth
+    childL !vertex = shiftL vertex 1
+    childR !vertex = (shiftL vertex 1) .|. 1
+
+-- | Evaluates parent values on `updateSegmentTree`.
+-- TODO: move to where clause?
+_evalToRoot ::
+  (Monoid a, MonoidAction op a, Eq op, VU.Unbox a, VU.Unbox op, PrimMonad m) =>
+  LazySegmentTree a op (PrimState m) ->
+  Int ->
+  m ()
+_evalToRoot (LazySegmentTree !as !ops !height) !iLeaf = do
+  let !leafVertex = iLeaf + nVerts `div` 2
+
+  forMS_ (rangeMS 1 (pred height)) $ \iParent -> do
+    let !vertex = nthParent leafVertex iParent
+
+    -- Evaluate this parent node by appending the child nodes:
+    !aL' <- mact <$> (VUM.read ops $ childL vertex) <*> (VUM.read as $ childL vertex)
+    !aR' <- mact <$> (VUM.read ops $ childR vertex) <*> (VUM.read as $ childR vertex)
+    VUM.write as vertex $ aL' <> aR'
+  where
+    !nVerts = VUM.length as
+    nthParent !leafVertex !nth = shiftR leafVertex nth
+    childL !vertex = shiftL vertex 1
+    childR !vertex = (shiftL vertex 1) .|. 1
+
+-- | Appends the lazy operator monoid monoids over some span of the lazy segment tree.
+-- | These values are just stored and performed over the nodes when queried.
+updateLazySTree ::
+  forall a op m.
+  (Monoid a, MonoidAction op a, Eq op, VU.Unbox a, VU.Unbox op, PrimMonad m) =>
+  (LazySegmentTree a op (PrimState m)) ->
+  Int ->
+  Int ->
+  op ->
+  m ()
+updateLazySTree stree@(LazySegmentTree !_ !ops !_) !iLLeaf !iRLeaf !op = do
+  -- 1. Propagate the parents' lazy operator monoids into the leaves:
+  _propOpMonoidsToLeaf stree iLLeaf
+  _propOpMonoidsToLeaf stree iRLeaf
+
+  -- 2. Propagate the given lazy operator monoids to the corresponding largest segments:
+  let !lVertex = iLLeaf + nVerts `div` 2
+      !rVertex = iRLeaf + nVerts `div` 2
+  glitchLoopUpdate lVertex rVertex
+
+  -- 3. Evaluate the parent vertices:
+  _evalToRoot stree iLLeaf
+  _evalToRoot stree iRLeaf
+
+  return ()
+  where
+    !nVerts = VUM.length ops
+
+    isLeftChild = not . (`testBit` 0)
+    isRightChild = (`testBit` 0)
+
+    -- Find the maximum segments for the given range and append the operator monoid.
+    -- It's much like using some glitch in a platformer game:
+    glitchLoopUpdate :: Int -> Int -> m ()
+    glitchLoopUpdate !l !r
+      | l > r = return ()
+      | otherwise = do
+          !l' <-
+            if isRightChild l
+              then do
+                VUM.modify ops (<> op) l
+                return $ succ l
+              else return l
+
+          !r' <-
+            -- NOTE: I'm using inclusive range
+            if isLeftChild r
+              then do
+                VUM.modify ops (<> op) r
+                return $ pred r
+              else return r
+
+          -- go up to the parent segment
+          glitchLoopUpdate (shiftR l' 1) (shiftR r' 1)
+
+-- TODO: I used the top-down queries for the strict segment tree. Which is preferable?
+queryLazySTree ::
+  forall a m op.
+  (Monoid a, MonoidAction op a, Eq op, VU.Unbox a, VU.Unbox op, PrimMonad m) =>
+  LazySegmentTree a op (PrimState m) ->
+  Int ->
+  Int ->
+  m a
+queryLazySTree stree@(LazySegmentTree !as !ops !_) !iLLeaf !iRLeaf = do
+  -- 1. Propagate the parents' lazy operator monoids into the leaves:
+  _propOpMonoidsToLeaf stree iLLeaf
+  _propOpMonoidsToLeaf stree iRLeaf
+
+  -- 2. Return concatanated result:
+  let !lVertex = iLLeaf + nVerts `div` 2
+      !rVertex = iRLeaf + nVerts `div` 2
+  glitchLoopQuery lVertex rVertex mempty mempty
+  where
+    !nVerts = VUM.length as
+
+    isLeftChild = not . (`testBit` 0)
+    isRightChild = (`testBit` 0)
+
+    -- Find the maximum segments for the given range and append the value.
+    -- It's much like using some glitch in a platformer game:
+    glitchLoopQuery :: Int -> Int -> a -> a -> m a
+    glitchLoopQuery !l !r !lAcc !rAcc
+      | l > r = return $ lAcc <> rAcc
+      | otherwise = do
+          (!l', !lAcc') <-
+            if isRightChild l
+              then do
+                -- Evaluate the target segmnent and append the result:
+                !la' <- mact <$> (VUM.read ops l) <*> (VUM.read as l)
+                return $ (succ l, lAcc <> la')
+              else return (l, lAcc)
+
+          (!r', !rAcc') <-
+            if isLeftChild r
+              then do
+                -- Evaluate the target segmnent and append the result:
+                !ra' <- mact <$> (VUM.read ops r) <*> (VUM.read as r)
+                return (pred r, ra' <> rAcc)
+              else return (r, rAcc)
+
+          -- go up to the parent segment
+          glitchLoopQuery (shiftR l' 1) (shiftR r' 1) lAcc' rAcc'
+
+-- Custom operator monoid template:
+--
+-- newtype Height = Height Int
+--   deriving (Show, Eq, Ord)
+--
+-- derivingUnbox
+--   "Height"
+--   [t|Height -> Int|]
+--   [|\(Height h) -> h|]
+--   [|\h -> Height h|]
+--
+-- instance Semigroup Height where
+--   h1 <> h2 = max h1 h2
+--
+-- instance Monoid Height where
+--   mempty = Height 0
+--   mconcat = maximum
+--
+-- instance MonoidAction Height Height where
+--   mact = max
+
+-- }}}
+
 -- {{{ Adhoc code
 
 data MyModulus = MyModulus
