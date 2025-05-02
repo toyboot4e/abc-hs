dea8e1a04b5c56639e04ee92114b3de8363354d4
toyboot4e <toyboot4e@gmail.com>
2025-05-02 19:50:27 +0900

    trie

--- abc403/e/Main.hs
+++ abc403/e/Main.hs
@@ -15,6 +15,7 @@ import ToyLib.Contest.Prelude
 -- import Data.ModInt
 -- import Data.PowMod
 -- import Data.Primes
+import Data.Trie
 
 -- }}} toy-lib import
 {-# RULES "Force inline VAI.sort" VAI.sort = VAI.sortBy compare #-}
@@ -23,129 +24,17 @@ debug :: Bool ; debug = False
 
 {- ORMOLU_ENABLE -}
 
-data Trie k v = Trie
-  { -- keyT :: {-# UNPACK #-} Int,
-    payloadT :: !v,
-    childrenT :: !(M.Map k (Trie k v))
-  }
-  deriving (Show, Eq)
-
--- | \(O(1)\)
-rootT :: (Ord k) => v -> Trie k v
-rootT x = Trie x M.empty
-
--- | \(O(k \log w)\)
-lookupT :: forall k v. (Ord k) => [k] -> Trie k v -> Maybe v
-lookupT = inner
-  where
-    inner :: [k] -> Trie k v -> Maybe v
-    inner [] !trie = Just $ payloadT trie
-    inner (k : keys) !trie = do
-      child <- M.lookup k $ childrenT trie
-      inner keys child
-
--- | \(O(k \log w)\)
-memberT :: forall k v. (Ord k) => [k] -> Trie k v -> Bool
-memberT ks = isJust . lookupT ks
-
--- | \(O(k \log w)\) Does nothing if no such vertex exist.
-modifyT :: forall k v. (Ord k) => (v -> v) -> [k] -> Trie k v -> Trie k v
-modifyT f = modifyNodeT (\(Trie p v) -> Trie (f p) v)
-
--- | \(O(k \log w)\) Does nothing if no such vertex exist.
-modifyNodeT :: forall k v. (Ord k) => (Trie k v -> Trie k v) -> [k] -> Trie k v -> Trie k v
-modifyNodeT f = inner
-  where
-    inner :: [k] -> Trie k v -> Trie k v
-    inner [] trie = f trie
-    inner (k : keys) trie = trie {childrenT = M.adjust (inner keys) k (childrenT trie)}
-
--- TODO: deduplicate the implementations of alloc*
-
--- | \(O(k \log w)\) Non-existing nodes in the path will have the same payload.
-allocPathT :: forall k v. (Ord k) => [k] -> v -> v -> Trie k v -> Trie k v
-allocPathT keys0 vParent vLeaf = inner keys0
-  where
-    inner :: [k] -> Trie k v -> Trie k v
-    inner [] trie = trie
-    inner (k : keys) (Trie payload children) =
-      -- TODO: fix it to one-pass
-      case M.lookup k children of
-        Just child -> Trie payload $! M.insert k (inner keys child) children
-        Nothing -> Trie payload $! M.insert k (allocPath keys) children
-    allocPath :: [k] -> Trie k v
-    allocPath [] = Trie vLeaf M.empty
-    allocPath (k : keys) = Trie vParent $! M.singleton k (allocPath keys)
-
--- | \(O(k \log w)\) Allocates a path from the root to a node and modifies the payload of the
--- target node.
-allocModifyT :: forall k v. (Ord k) => (v -> v) -> [k] -> v -> Trie k v -> Trie k v
-allocModifyT f keys0 vDefault = inner keys0
-  where
-    inner :: [k] -> Trie k v -> Trie k v
-    inner [] trie = trie { payloadT = f (payloadT trie) }
-    inner (k : keys) (Trie payload children) =
-      -- TODO: fix it to one-pass
-      case M.lookup k children of
-        Just child -> Trie payload $! M.insert k (inner keys child) children
-        Nothing -> Trie payload $! M.insert k (allocPath keys) children
-    allocPath :: [k] -> Trie k v
-    allocPath [] = Trie (f vDefault) M.empty
-    allocPath (k : keys) = Trie vDefault $! M.singleton k (allocPath keys)
-
--- | \(O(k \log w)\) Allocates a path from the root to a node and modifies the payload of the node
--- in the path.
-allocModifyPathT :: forall k v. (Ord k) => (v -> v) -> [k] -> v -> Trie k v -> Trie k v
-allocModifyPathT f keys0 vDefault = inner keys0
-  where
-    !fv = f vDefault
-    inner :: [k] -> Trie k v -> Trie k v
-    inner [] trie = trie { payloadT = f (payloadT trie) }
-    inner (k : keys) (Trie payload children) =
-      case M.lookup k children of
-        Just child -> Trie payload' $! M.insert k (inner keys child) children
-        Nothing -> Trie payload' $! M.insert k (allocPath keys) children
-      where
-        !payload' = f payload
-    allocPath :: [k] -> Trie k v
-    allocPath [] = Trie fv M.empty
-    allocPath (k : keys) = Trie fv $! M.singleton k (allocPath keys)
-
--- | \(O(k \log w)\) Modifies payload of a node and their parents.
---
--- ==== Constraints
--- - The key must be in the map.
-modifyPathT :: forall k v. (Ord k) => (v -> v) -> [k] -> Trie k v -> Trie k v
-modifyPathT f = inner
-  where
-    inner :: [k] -> Trie k v -> Trie k v
-    inner [] !trie = trie {payloadT = f (payloadT trie)}
-    inner (k : keys) !trie = Trie (f (payloadT trie)) (M.adjust (inner keys) k (childrenT trie))
-
--- | \(O(k \log w)\) Returns prefix payloads, excluding the root.
-pathT :: forall k v. (Ord k) => [k] -> Trie k v -> [v]
-pathT = inner
-  where
-    inner :: [k] -> Trie k v -> [v]
-    inner [] !trie = []
-    inner (k : keys) !trie = do
-      -- TODO: write monadic code
-      case M.lookup k (childrenT trie) of
-        Just child -> (payloadT child :) $ inner keys child
-        Nothing -> []
-
 data Payload = Payload
   { -- | Represents whether the prefix is acceptable.
     acceptableP :: !Bool,
     -- | Query indices that match to the prefix.
-    matchesP :: !IS.IntSet
+    matchesP :: !(S.Set Int)
   }
   deriving (Show, Eq)
 
 insertP :: Int -> Payload -> Payload
-insertP k (Payload b is) = Payload b $! IS.insert k is
+insertP k (Payload b is) = Payload b $! S.insert k is
 
--- | Rolling hash answer
 solve :: StateT BS.ByteString IO ()
 solve = do
   !q <- int'
@@ -153,18 +42,18 @@ solve = do
 
   let !res = V.tail $ V.iscanl' step s0 qs
         where
-          !s0 = (rootT @Char @Payload (Payload False IS.empty), IS.empty)
-          !p0 = Payload False IS.empty
-          !p1 = Payload True IS.empty
+          !s0 = (rootT @Char @Payload (Payload False S.empty), S.empty)
+          !p0 = Payload False S.empty
+          !p1 = Payload True S.empty
 
           -- add prefix
-          step :: Int -> (Trie Char Payload, IS.IntSet) -> (Int, BS.ByteString) -> (Trie Char Payload, IS.IntSet)
+          step :: Int -> (Trie Char Payload, S.Set Int) -> (Int, BS.ByteString) -> (Trie Char Payload, S.Set Int)
           step iQuery (!trie, !rest) (1, !s) = (trie', rest')
             where
-              !trie' = allocModifyT (const p1) (BS.unpack s) (Payload False IS.empty) trie
+              !trie' = allocModifyT (const p1) (BS.unpack s) p0 trie
               !rest' = fromMaybe rest $ do
                 payload <- lookupT (BS.unpack s) trie
-                pure $! IS.difference rest $ matchesP payload
+                pure $! S.difference rest $ matchesP payload
 
           -- add target
           step iQuery (!trie, !rest) (2, !s) = (trie', rest')
@@ -175,10 +64,10 @@ solve = do
                 | otherwise = allocModifyPathT (insertP iQuery) (BS.unpack s) p0 trie
               !rest'
                 | anyMatch = rest
-                | otherwise = IS.insert iQuery rest
+                | otherwise = S.insert iQuery rest
           step _ _ _ = error "unreachable"
 
-  printBSB $ unlinesBSB $ V.imap (\i (!_, !rest) -> IS.size rest) res
+  printBSB $ unlinesBSB $ V.imap (\i (!_, !rest) -> S.size rest) res
 
 -- verification-helper: PROBLEM https://atcoder.jp/contests/abc403/tasks/abc403_e
 main :: IO ()
