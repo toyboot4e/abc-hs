515d3f5b409c3f6b27c0b8c53e8537c468081d02
toyboot4e <toyboot4e@gmail.com>
2025-09-17 22:10:34 +0900

    update `flake.nix`

--- flake.nix
+++ flake.nix
@@ -19,37 +19,13 @@
           src = pkgs.fetchFromGitHub {
             owner = "Tatamo";
             repo = "atcoder-cli";
-            rev = "f385e71ba270716f5a94e3ed9bd23a24f78799d0";
-            sha256 = "sha256-7pbCTgWt+khKVyMV03HanvuOX2uAC0PL9OLmqly7IWE=";
+            tag = "v2.2.0";
+            hash = "sha256-7pbCTgWt+khKVyMV03HanvuOX2uAC0PL9OLmqly7IWE=";
           };
-          npmDepsHash = "sha256-ufG7Fq5D2SOzUp8KYRYUB5tYJYoADuhK+2zDfG0a3ks=";
-          npmPackFlags = [ "--ignore-scripts" ];
+          npmDepsHash = "sha256-ufG7Fq5D2SOzUp8KYRYUB5tYJYoADuhK+2zDfG0a3ks";
+          npmFlags = [ "--ignore-scripts" ];
           NODE_OPTIONS = "--openssl-legacy-provider";
         };
-        oj-verify =
-          with pkgs.python3Packages;
-          pkgs.python3Packages.buildPythonApplication {
-            name = "verification-helper";
-            version = "5.6.0";
-            pyproject = true;
-            src = pkgs.fetchFromGitHub {
-              owner = "online-judge-tools";
-              repo = "verification-helper";
-              rev = "adbff121b1f96de5f34e9f1483eb47d661c54075";
-              fetchSubmodules = false;
-              sha256 = "sha256-f7Ge8kLRQv9uxdNGtgNsypGVY0XAnKPCg8HYQ5nT6mI=";
-            };
-            build-system = [ setuptools ];
-            dependencies = [
-              colorlog
-              importlab
-              online-judge-tools
-              pyyaml
-              setuptools
-              toml
-            ];
-            propagatedBuildInputs = [ setuptools ];
-          };
       in
       {
         devShells.default =
@@ -65,7 +41,7 @@
             packages = [
               atcoder-cli
               online-judge-tools
-              oj-verify
+              online-judge-verify-helper
               just
 
               # TODO: needed?
@@ -76,10 +52,10 @@
               # nodejs
 
               hlint
-              haskell.compiler.ghc947
-              (haskell-language-server.override { supportedGhcVersions = [ "947" ]; })
-              haskell.packages.ghc947.cabal-fmt
-              haskell.packages.ghc947.doctest
+              haskell.compiler.ghc948
+              (haskell-language-server.override { supportedGhcVersions = [ "948" ]; })
+              haskell.packages.ghc948.cabal-fmt
+              haskell.packages.ghc948.doctest
               haskellPackages.hoogle
               haskellPackages.ghcid
               haskellPackages.ghcide
